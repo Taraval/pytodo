9df8235d065b63302cde8f5c1e5dd58738807f4d
OneTwoThree <paulpc.dev@gmail.com>
2018-01-22 23:29:07 -0800

    Update todo.py

--- todo.py
+++ todo.py
@@ -11,7 +11,14 @@ DONE = __location__ + "done.txt"
 
 
 def help_menu():
-    print("Please see documentation for more help.")
+    print("""t <action> <#> <value>
+
+    add              - add a task
+    list             - list all the task
+    update           - update a task number
+    priority         - set the priority of the task
+    done/complete    - mark task completed
+    """)
 
 
 def task_update():
@@ -34,16 +41,26 @@ def task_delete():
 
 
 def task_complete():
-    item = argv[2]
-    content = mod_task_read(TODO)
-    new_content = content[int(item) - 1]
-    del content[int(item) - 1]
-    mod_task_write(TODO, content)
-    content_done = mod_task_read(DONE)
-    content_done.append(new_content)
-    print("--")
-    print("Todo task item " + item + " marked completed.")
-    mod_task_write(DONE, content_done)
+    try:
+        item = argv[2]
+    except:
+        item = None
+    if item != None:
+        content = mod_task_read(TODO)
+        new_content = content[int(item) - 1]
+        del content[int(item) - 1]
+        mod_task_write(TODO, content)
+        content_done = mod_task_read(DONE)
+        content_done.append(new_content)
+        print("--")
+        print("Todo task item " + item + " marked completed.")
+        mod_task_write(DONE, content_done)
+    else:
+        content = mod_task_read(DONE)
+        for idx, x in enumerate(content):
+            print(str(idx + 1) + " " + x.strip())
+        print("--")
+        print("DONE: " + str(len(content)) + " task completed")
 
 
 def task_list():
@@ -55,13 +72,13 @@ def task_list():
     if item != None:
         content = mod_task_read(TODO)
         new_content = content[int(item) - 1]
-        print item + " " + new_content.strip()
+        print(item + " " + new_content.strip())
         print("--")
         print("TODO: " + str(len(content)) + " task total")
     else:
         content = mod_task_read(TODO)
         for idx, x in enumerate(content):
-            print str(idx + 1) + " " + x.strip()
+            print(str(idx + 1) + " " + x.strip())
         print("--")
         print("TODO: " + str(len(content)) + " task total")
 
@@ -115,10 +132,10 @@ def mod_task_write(_file_, content):
 """ Mapping the action key words """
 cmd_map = {"add": task_add, "ad": task_add, "a": task_add,
            "list": task_list, "ls": task_list, "l": task_list,
-           "delete": task_delete, "del": task_delete, "d": task_delete,
+           "delete": task_delete, "del": task_delete, "de": task_delete,
            "update": task_update, "up": task_update, "u": task_update,
            "priority": task_priority, "pri": task_priority, "p": task_priority,
-           "complete": task_complete, "com": task_complete, "c": task_complete
+           "complete": task_complete, "com": task_complete, "c": task_complete, "done": task_complete, "d": task_complete
            }
 
 
