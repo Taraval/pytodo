88908916b4dab124f4192c7b4697b522fba59777
OneTwoThree <paulpc.dev@gmail.com>
2018-01-27 12:42:05 -0800

    Updated todo.py - Added workspace

--- todo.py
+++ todo.py
@@ -4,20 +4,45 @@ import os
 import re
 from sys import argv
 
-__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+if os.environ.get('PYTODO'):
+    __location__ = os.environ.get('PYTODO')
+else:
+    __location__ = os.environ.get('HOME') + "/.pytodo"
 
-TODO = __location__ + "todo.txt"
-DONE = __location__ + "done.txt"
+WS_FILE = __location__ + "/workspace"
+WS_DEFAULT = "work"
+
+if not os.path.exists(__location__):
+    os.makedirs(__location__)
+
+if os.path.isfile(WS_FILE):
+    with open(WS_FILE) as f:
+        WS_DEFAULT = f.read()
+    f.closed
+else:
+    default_dir = open(WS_FILE,"w")
+    default_dir.write(WS_DEFAULT)
+    default_dir.close()
+
+WS_STORE = __location__ + "/" + WS_DEFAULT
+
+if not os.path.exists(WS_STORE):
+    os.makedirs(WS_STORE)
+
+TODO = WS_STORE + "/todo.txt"
+DONE = WS_STORE + "/done.txt"
 
 
 def help_menu():
     print("""t <action> <#> <value>
 
-    add              - add a task
-    list             - list all the task
-    update           - update a task number
-    priority         - set the priority of the task
-    done/complete    - mark task completed
+    add, ad, a           - add a task
+    list, ls, l          - list all the task
+    update, u            - update a task number
+    priority, pri, p     - set the priority of the task
+    done/complete, do    - mark task completed
+    workspace, ws, w     - list/create/change workspace 
+    workspacedelete, wd  - delete workspace
     """)
 
 
@@ -115,6 +140,67 @@ def task_priority():
         print("Please RTFM")
 
 
+def task_workspace():
+    try:
+        item = argv[2]
+    except:
+        item = None
+
+    if item != None:
+        WS_STORE = __location__ + "/" + item
+        if not os.path.exists(WS_STORE):
+            print("No %s workspace, do you want to create? Please enter (y/n) " %item)
+            ws_new = False
+            yes = {'yes','y', 'ye', ''}
+            choice = raw_input().lower()
+            if choice in yes:
+                ws_new = True
+            if ws_new:
+                os.makedirs(WS_STORE)
+                default_dir = open(WS_FILE,"w")
+                default_dir.write(item)
+                default_dir.close()
+                print("--")
+                print("WORKSPACE: [%s]" %tem)
+        else:
+            default_dir = open(WS_FILE,"w")
+            default_dir.write(item)
+            default_dir.close()
+            print("--")
+            print("WORKSPACE: [%s]" %item)
+    else:
+        with open(WS_FILE) as f:
+            WS_DEFAULT = f.read()
+        f.closed
+        print("ACTIVE WORKSPACE: [%s]" %WS_DEFAULT)
+        print("--")
+        ws_list = ""
+        for root, dirs, files in os.walk(__location__):
+            if dirs:
+                ws_list += ', '.join(dirs)
+        print("AVAILABLE WORKSPACES: [%s]" %ws_list)
+
+def task_workspacedelete():
+    try:
+        item = argv[2]
+    except:
+        item = None
+
+    if item != None:
+        WS_STORE = __location__ + "/" + item
+        if os.path.exists(WS_STORE):
+            print("Do you want to delete [%s] workspace? Please enter (y/n) " %item)
+            ws_delete = False
+            yes = {'yes','y', 'ye', ''}
+            choice = raw_input().lower()
+            if choice in yes:
+                ws_delete= True
+            if ws_delete:
+                os.rmdir(WS_STORE)
+    else:
+        print("Please recareful on deleting workspace")
+ 
+
 def mod_task_read(_file_):
     if os.path.exists(_file_):
         with open(_file_, "r") as todoList:
@@ -135,7 +221,9 @@ cmd_map = {"add": task_add, "ad": task_add, "a": task_add,
            "delete": task_delete, "del": task_delete, "de": task_delete,
            "update": task_update, "up": task_update, "u": task_update,
            "priority": task_priority, "pri": task_priority, "p": task_priority,
-           "complete": task_complete, "com": task_complete, "c": task_complete, "done": task_complete, "d": task_complete
+           "complete": task_complete, "com": task_complete, "c": task_complete, "done": task_complete, "d": task_complete,
+           "workspace": task_workspace, "ws": task_workspace, "w": task_workspace,
+           "workspacedelete": task_workspacedelete, "wsdelete": task_workspacedelete, "wd": task_workspacedelete
            }
 
 
@@ -148,5 +236,4 @@ def main():
     cmd_map.get(get_cmd, help_menu)()
 
 if __name__ == "__main__":
-    #print ("Calling Main")
     main()
